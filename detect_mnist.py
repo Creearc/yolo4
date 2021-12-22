4bfc69fb0948430031e090947db79c35b8eef7f1
Alexandr <knxandr@rambler.ru>
2021-12-23 02:50:27 +0500

    Update detect_mnist.py

--- detect_mnist.py
+++ detect_mnist.py
@@ -19,16 +19,21 @@ from yolov3.yolov4 import Create_Yolo
 from yolov3.utils import detect_video
 from yolov3.configs import *
 
-video_path = '/home/alexandr/tests/WhatsApp Video 2021-11-12 at 15.29.20.mp4'
-
+path = '/home/alexandr/tests/'
+files = ['WhatsApp Video 2021-11-12 at 15.29.20.mp4',
+         'WhatsApp Video 2021-11-12 at 15.29.30.mp4',
+         'WhatsApp Video 2021-11-12 at 15.29.39.mp4',
+         'WhatsApp Video 2021-11-12 at 15.29.51.mp4',
+         ]
 
 yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
 #yolo.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}") # use keras weights
 yolo.load_weights(f"./checkpoints/yolov4_custom_Tiny")
 
-##    detect_image(yolo, image_path, "mnist_test.jpg", input_size=YOLO_INPUT_SIZE,
-##                 show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
-detect_video(yolo, video_path, './IMAGES/detected.mp4', input_size=YOLO_INPUT_SIZE,
-             show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+for i in range(len(files)):
+    print(files[i])
+    video_path = '{}{}'.format(path, files[i])
+    detect_video(yolo, video_path, './IMAGES/{}.mp4'.format(i), input_size=YOLO_INPUT_SIZE,
+                 show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
 
     
