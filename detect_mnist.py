36539b91648e8fd7f4882affcf933893fc612e54
Alexandr <knxandr@rambler.ru>
2021-12-23 02:37:36 +0500

    Update detect_mnist.py

--- detect_mnist.py
+++ detect_mnist.py
@@ -29,6 +29,9 @@ while True:
     yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
     #yolo.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}") # use keras weights
     yolo.load_weights(f"./checkpoints/yolov4_custom_Tiny")
+    print(1)
+    break
 
     detect_image(yolo, image_path, "mnist_test.jpg", input_size=YOLO_INPUT_SIZE,
                  show=True, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+    
