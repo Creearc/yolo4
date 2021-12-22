17b911312e42951b1915f12aaedf9a2d47af310a
Alexandr <knxandr@rambler.ru>
2021-12-22 20:34:58 +0500

    Update configs.py

--- yolov3/configs.py
+++ yolov3/configs.py
@@ -10,7 +10,7 @@
 #================================================================
 
 # YOLO options
-YOLO_TYPE                   = "yolov3" # yolov4 or yolov3
+YOLO_TYPE                   = "yolov4" # yolov4 or yolov3
 YOLO_FRAMEWORK              = "tf" # "tf" or "trt"
 YOLO_V3_WEIGHTS             = "model_data/yolov3.weights"
 YOLO_V4_WEIGHTS             = "model_data/yolov4.weights"
@@ -34,7 +34,7 @@ if YOLO_TYPE                == "yolov3":
                                [[30,  61], [62,   45], [59,  119]],
                                [[116, 90], [156, 198], [373, 326]]]
 # Train options
-TRAIN_YOLO_TINY             = False
+TRAIN_YOLO_TINY             = not False
 TRAIN_SAVE_BEST_ONLY        = True # saves only best model according validation loss (True recommended)
 TRAIN_SAVE_CHECKPOINT       = False # saves all best validated checkpoints in training process (may require a lot disk space) (False recommended)
 TRAIN_CLASSES               = "mnist/mnist.names"
@@ -43,7 +43,7 @@ TRAIN_LOGDIR                = "log"
 TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
 TRAIN_MODEL_NAME            = f"{YOLO_TYPE}_custom"
 TRAIN_LOAD_IMAGES_TO_RAM    = True # With True faster training, but need more RAM
-TRAIN_BATCH_SIZE            = 4
+TRAIN_BATCH_SIZE            = 64
 TRAIN_INPUT_SIZE            = 416
 TRAIN_DATA_AUG              = True
 TRAIN_TRANSFER              = True
@@ -55,7 +55,7 @@ TRAIN_EPOCHS                = 100
 
 # TEST options
 TEST_ANNOT_PATH             = "mnist/mnist_test.txt"
-TEST_BATCH_SIZE             = 4
+TEST_BATCH_SIZE             = 64
 TEST_INPUT_SIZE             = 416
 TEST_DATA_AUG               = False
 TEST_DECTECTED_IMAGE_PATH   = ""
