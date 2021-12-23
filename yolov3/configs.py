bee35c8f17ba2296460afdb13f6c67eb133e2a29
Alexandr <knxandr@rambler.ru>
2021-12-23 16:52:30 +0500

    Update configs.py

--- yolov3/configs.py
+++ yolov3/configs.py
@@ -24,7 +24,7 @@ YOLO_STRIDES                = [8, 16, 32]
 YOLO_IOU_LOSS_THRESH        = 0.5
 YOLO_ANCHOR_PER_SCALE       = 3
 YOLO_MAX_BBOX_PER_SCALE     = 100
-YOLO_INPUT_SIZE             = 416
+YOLO_INPUT_SIZE             = 416 * 2
 if YOLO_TYPE                == "yolov4":
     YOLO_ANCHORS            = [[[12,  16], [19,   36], [40,   28]],
                                [[36,  75], [76,   55], [72,  146]],
@@ -44,7 +44,7 @@ TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
 TRAIN_MODEL_NAME            = f"yolov4_custom"
 TRAIN_LOAD_IMAGES_TO_RAM    = not True # With True faster training, but need more RAM
 TRAIN_BATCH_SIZE            = 32
-TRAIN_INPUT_SIZE            = 416
+TRAIN_INPUT_SIZE            = 416 * 2
 TRAIN_DATA_AUG              = True
 TRAIN_TRANSFER              = True
 TRAIN_FROM_CHECKPOINT       = not False # "checkpoints/yolov3_custom"
@@ -56,7 +56,7 @@ TRAIN_EPOCHS                = 20
 # TEST options
 TEST_ANNOT_PATH             = "/home/alexandr/tmp/bottles_2/dataset_test.txt"
 TEST_BATCH_SIZE             = 32
-TEST_INPUT_SIZE             = 416
+TEST_INPUT_SIZE             = 416 * 2
 TEST_DATA_AUG               = False
 TEST_DECTECTED_IMAGE_PATH   = ""
 TEST_SCORE_THRESHOLD        = 0.7
